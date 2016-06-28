02b29fceb7956f03217b93de86cc5b86a1ff13af
Matthew Howland <howlandm@gmail.com>
2016-06-28 18:39:24 -0400

    finished blank lightbright

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,13 @@
 $(document).ready(function(){
   var container = $('.container');
   var numOfRows = 10;
-  var numOfCols= 10;
+  var numOfCols = 10;
 
   initGrid();
   function initGrid(){
     for(var i = 0; i< numOfRows; i += 1){
       var row = $('<div></div>');
+      row.addClass('row');
       for(var j = 0; j < numOfCols; j += 1){
         var cell = $('<div></div>');
         cell.addClass ('cell border');
